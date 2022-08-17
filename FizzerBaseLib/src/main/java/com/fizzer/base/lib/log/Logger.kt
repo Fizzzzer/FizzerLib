6da5db10cd6db5108fdf9e81130a199b149148d4
Fizzer <doraemonma503@gmail.com>
2022-08-17 22:58:21 +0800

    添加

---
+++ FizzerBaseLib/src/main/java/com/fizzer/base/lib/log/Logger.kt
@@ -0,0 +1,17 @@
+package com.fizzer.base.lib.log
+
+import android.util.Log
+
+/**
+ * @Author fizzer
+ * @Data 2022/8/17 - 10:54 下午
+ * @Email Fizzer53@sina.com
+ * @Describe:
+ */
+class Logger {
+    companion object{
+        fun printLog(){
+            Log.e("Fizzer","打印log")
+        }
+    }
+}
