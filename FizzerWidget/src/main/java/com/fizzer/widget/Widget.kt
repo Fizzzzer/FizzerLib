4609f2cfcee0b9e0464c6e0424d3417ad0380d53
Fizzer <doraemonma503@gmail.com>
2022-08-17 23:18:23 +0800

    提交

---
+++ FizzerWidget/src/main/java/com/fizzer/widget/Widget.kt
@@ -0,0 +1,17 @@
+package com.fizzer.widget
+
+import android.util.Log
+
+/**
+ * @Author fizzer
+ * @Data 2022/8/17 - 11:16 下午
+ * @Email Fizzer53@sina.com
+ * @Describe:
+ */
+class Widget {
+    companion object{
+        fun printLog(){
+            Log.e("Fizzer","Widget")
+        }
+    }
+}
