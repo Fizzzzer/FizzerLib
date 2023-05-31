e93afa3021d2e944faf2760efe55b8cf8e50f270
Fizzer_Tinno <fizzer503@gmail.com>
2023-05-31 11:53:47 +0800

    修改module为Simple

--- app/src/main/java/com/fizzer/lib/MyApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.fizzer.lib
-
-import android.app.Application
-
-class MyApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
---
+++ simple/src/main/java/com/fizzer/lib/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.fizzer.lib
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
