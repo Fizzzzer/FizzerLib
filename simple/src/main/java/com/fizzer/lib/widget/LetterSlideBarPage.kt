cfde60638cae270a9f694fcebc4259ce28a435fc
Fizzer <fizzer503@gmail.com>
2023-12-14 18:10:29 +0800

    添加数字索引

---
+++ simple/src/main/java/com/fizzer/lib/widget/LetterSlideBarPage.kt
@@ -0,0 +1,10 @@
+package com.fizzer.lib.widget
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LetterSlideBarPage :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
