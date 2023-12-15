0abe32166cd04cbd7d148cdca7ed6eddb7bd4f0f
Fizzer <fizzer503@gmail.com>
2023-12-16 00:00:18 +0800

    添加Android对话框的基类

---
+++ FizzerBaseLib/src/main/java/com/fizzer/base/lib/android/BaseViewModel.kt
@@ -0,0 +1,12 @@
+package com.fizzer.base.lib.android
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @Author fizzer
+ * @Data 2021/7/15 - 12:33 上午
+ * @Email Fizzer53@sina.com
+ * @Describe:
+ */
+class BaseViewModel : ViewModel(){
+}
